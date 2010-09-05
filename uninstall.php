2d5fb10c3d79d0f47cdb58c154e981ac86edb8dd
elusivelight <elusivelight@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-05 18:18:30 +0000

    git-svn-id: https://plugins.svn.wordpress.org/cleverness-to-do-list/tags/2.1.5@285478 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
+    exit();
+
+if ( current_user_can('delete_plugins') ) {
+
+	delete_option( 'cleverness_todo_settings' );
+	delete_option( 'cleverness_todo_db_version' );
+	delete_option( 'atd_db_version' );
+
+  	global $wpdb;
+  	$thetable = $wpdb->prefix."todolist";
+  	$wpdb->query("DROP TABLE IF EXISTS $thetable");
+  	$thecattable = $wpdb->prefix."todolist_cats";
+  	$wpdb->query("DROP TABLE IF EXISTS $thecattable");
+}
+?>
