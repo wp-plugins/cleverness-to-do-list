001b746fdf9f3c422d908b4e992f24dc869557ba
elusivelight <elusivelight@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-29 02:34:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/cleverness-to-do-list/trunk@512076 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cleverness-to-do-list.php
+++ cleverness-to-do-list.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Cleverness To-Do List
-Version: 3.0.5
+Version: 3.0.6
 Description: Manage to-do list items on a individual or group basis with categories. Includes a dashboard widget and a sidebar widget.
 Author: C.M. Kendrick
 Author URI: http://cleverness.org
@@ -27,7 +27,7 @@ include_once 'includes/cleverness-to-do-list-widget.class.php';
 function cleverness_todo_loader() {
 
 	define( 'CTDL_DB_VERSION', '3.0.3' ); // also update in install function in library
-	define( 'CTDL_PLUGIN_VERSION', '3.0.5' );
+	define( 'CTDL_PLUGIN_VERSION', '3.0.6' );
 	define( 'CTDL_FILE', __FILE__ );
 	define( 'CTDL_BASENAME', plugin_basename( __FILE__ ) );
 	define( 'CTDL_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
